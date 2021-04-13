1e042f6dfa2d324fda66112d84217b64f9b24570
William Martini <william.martini@outlook.com>
2021-04-13 09:05:52 +0200

    New structure

--- dist/src/business-partner.controller.d.ts
+++
@@ -1,4 +0,0 @@
-import { BusinessPartner } from "@sap/cloud-sdk-vdm-business-partner-service";
-export declare class BusinessPartnerController {
-    getBusinessPartners(): Promise<BusinessPartner[]>;
-}
